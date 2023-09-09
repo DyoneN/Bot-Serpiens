212f2c5436b9a54d0ebd3ff7282490d79301527c
DyoneN <63010260+DyoneN@users.noreply.github.com>
2023-09-09 16:09:41 -0300

    First commit
    Developer Dyone Andrade

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+import './globals.css'
+import type { Metadata } from 'next'
+
+
+export const metadata: Metadata = {
+  title: 'Bot Serpiens',
+  description: 'O primeiro Bot que integra tudo1',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className="bg-[#F5F6F8]" >{children}</body>
+    </html>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Header from "@/components/Header"
+
+export default function Home() {
+  return (
+    <main>
+      <Header />
+
+      {/** Board */}
+     <h1>Bot Serpiens IA 3.5</h1>
+    </main>
+  )
+}
